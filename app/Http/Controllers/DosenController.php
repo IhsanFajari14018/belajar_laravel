158d2f5f08a82d4c6daee749cc9039aa9633c918
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-01-27 14:21:55 +0700

    Update DosenController.php

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class DosenController extends Controller{
-    //
+
     public function index(){
-      return "Halo ini adalah method index, dalam controller DosenController";
+      // return "Halo ini adalah method index, dalam controller DosenController";
+      $nama = "Ihsan Fajari";
+      $pelajaran = ["Management Project", "Web Programming"];
+      return view('biodata', ['nama' => $nama, 'daftar_matkul' => $pelajaran]);
     }
+
 }
