c8f35b151c1ecedb68ea0d072f3be6252af65ec8
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-01-27 16:40:17 +0700

    Update formulir.blade.php

--- resources/views/formulir.blade.php
+++ resources/views/formulir.blade.php
@@ -7,7 +7,7 @@
 
   <form action="/formulir/proses" method="post">
     <input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>">
-    
+
     Nama :
     <input type="text" name="nama"> <br/>
     Alamat :
