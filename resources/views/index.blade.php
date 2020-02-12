6b80b5af78c5f3888791aa3ade9728a335c87cad
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-02-12 13:41:53 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,64 +3,114 @@
 <head>
   <title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
 
+  <!-- STYLING -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/bootstrap.min.css') }}">
+
+
   <style type ="text/css">
   .pagination li{
     float: left;
     list-style-type: none;
-    margin: 5px;
+    margin: 0px;
   }
   </style>
+
+  <!-- END OF STYLING -->
 </head>
-<body>
+<body class="bg-light">
 
-  <h2>www.malasngoding.com</h2>
-  <h3>Data Pegawai</h3>
+  <header>
 
-  <p>Cari Data Pegawai :</p>
-  <p>Previous Search: {{ old('cari') }}</p>
-  <form role="form" action="/pegawai/cari" method="GET">
-    <input type="text" name="cari"  value="{{ old('cari') }}">
-    <input type="submit" value="CARI">
-  </form>
+  </header>
 
-  <br>
+  <!-- ALL MAIN CONTENT -->
+  <main>
 
-  <a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
+    <!-- MAIN TITLE -->
+    <section class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h2 class="display-2">www.malasngoding.com</h2>
+        </div>
+        <div class="col-12">
+          <h3>Data Pegawai</h3>
+        </div>
+      </div>
+    </section>
 
-  <br/>
-  <br/>
+    <!-- MAIN DATA CONTENT -->
+    <section class="container mt-5">
 
-  <table border="1">
-    <tr>
-      <th>Nama</th>
-      <th>Jabatan</th>
-      <th>Umur</th>
-      <th>Alamat</th>
-      <th>Opsi</th>
-    </tr>
-    @foreach($pegawai as $p)
-    <tr>
-      <td>{{ $p->nama }}</td>
-      <td>{{ $p->jabatan }}</td>
-      <td>{{ $p->umur }}</td>
-      <td>{{ $p->alamat }}</td>
-      <td>
-        <a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
-        |
-        <a href="/pegawai/delete/{{ $p->pegawai_id }}">Hapus</a>
-      </td>
-    </tr>
-    @endforeach
-  </table>
+      <!-- TAGLINE & CURRENT SEARCH -->
+      <p>
+        Cari Data Pegawai <br>
+        Previous Search: {{ old('cari') }}
+      </p>
 
-  <br/>
+      <!-- SEARCH BOX -->
+      <form role="form" action="/pegawai/cari" method="GET">
+        <div class="form-group row">
+          <div class="col-2 pr-0">
+            <input type="text" class="form-control" name="cari"  value="{{ old('cari') }}">
+          </div>
+          <div class="col-4 pl-0">
+            <input type="submit" class="btn btn-info" value="CARI">
+          </div>
+        </div>
+      </form>
 
-  Halaman: {{$pegawai->currentPage()}} <br/>
-  Jumlah Data: {{$pegawai->total()}} <br/>
-  Data Per Halaman: {{$pegawai->perPage()}} <br/>
+      <br>
 
-  {{$pegawai->links()}}
+      <!-- ADD NEW PEGAWAI -->
+      <a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
 
+      <br/>
+      <br/>
+
+      <!-- DISPLAY PEGAWAI IN TABLE -->
+      <div class="table-responsive bg-white">
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th>Nama</th>
+              <th>Jabatan</th>
+              <th>Umur</th>
+              <th>Alamat</th>
+              <th>Opsi</th>
+            </tr>
+          </thead>
+          @foreach($pegawai as $p)
+          <tr>
+            <td>{{ $p->nama }}</td>
+            <td>{{ $p->jabatan }}</td>
+            <td>{{ $p->umur }}</td>
+            <td>{{ $p->alamat }}</td>
+            <td>
+              <a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
+              |
+              <a href="/pegawai/delete/{{ $p->pegawai_id }}">Hapus</a>
+            </td>
+          </tr>
+          @endforeach
+        </table>
+      </div>
+
+      <br/>
+
+      Halaman: {{$pegawai->currentPage()}} <br/>
+      Jumlah Data: {{$pegawai->total()}} <br/>
+      Data Per Halaman: {{$pegawai->perPage()}} <br/>
+
+      <!-- PAGINATION -->
+      <div class="mt-3">
+        {{$pegawai->links()}}
+      </div>
+
+    </section>
+
+  </main>
+
+  <script type="text/javascript" src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
 
 </body>
 </html>
