938123326710f5061e8865d0230eeafa56a518d0
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-02-13 16:24:06 +0700

    Update PegawaiController.php

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -11,21 +11,68 @@ use App\Pegawai;
 class PegawaiController extends Controller{
 
   public function index(){
-    // $pegawai = Pegawai::all();
+    $pegawai = Pegawai::all();
     // $pegawai = Pegawai::first();
     // $pegawai = Pegawai::where('id', '1')->get();
     // $pegawai = Pegawai::where('id', '<=','10')->get();
     // $pegawai = Pegawai::where('nama', 'like', '%y%')->get();
-    $pegawai = Pegawai::paginate(4);
+    // $pegawai = Pegawai::paginate(4);
 
-    return view('index_2', ['pegawai' => $pegawai]);
+    // return view('index_2', ['pegawai' => $pegawai]);
+    return view('pegawai', ['pegawai' => $pegawai]);
     // return $pegawai;
   }
 
+  /* method untuk menampilkan view form tambah pegawai */
+  public function tambah(){
+    return view('tambah');
+  }
 
+  /* model method - with query builder */
+  public function store(Request $request){
+    // insert data to pegawai table
+    $this->validate($request,[
+      'nama'    =>  'required',
+      'alamat'  =>  'required'
+    ]);
 
+    Pegawai::create([
+      'nama'    => $request->nama,
+      'alamat'  => $request->alamat
+    ]);
 
+    return redirect('/pegawai');
+  }
 
+  public function edit($id){
+    $pegawai = Pegawai::find($id);
+    return view('pegawai_edit', ['pegawai' => $pegawai]);
+  }
+
+  /* model method - update pegawai by id */
+  public function update($id, Request $request){
+    //validate before submitting
+    $this->validate($request, [
+      'nama'    => 'required',
+      'alamat'  => 'required'
+    ]);
+
+    //Step to update data pegawai with Model:
+    $pegawai = Pegawai::find($id);
+    $pegawai->nama = $request->nama;
+    $pegawai->alamat = $request->alamat;
+    $pegawai->save();
+
+    return redirect('/pegawai');
+  }
+
+  public function delete($id){
+    $pegawai = Pegawai::find($id);
+    $pegawai->delete();
+
+    // return redirect('/pegawai');
+    return redirect()->back();
+  }
 
 
 
@@ -50,23 +97,20 @@ class PegawaiController extends Controller{
     // $pegawai = DB::table('pegawai')->get();
 
     // WITH PAGINATION
-    // mengambil data dari table pegawai
-    // $pegawai = DB::table('pegawai')->paginate(10);
-    $pegawai = Pegawai::all();
+    // mengambil data dari table pegawai with Query Builder
+    $pegawai = DB::table('pegawai')->paginate(10);
 
     // mengirim data pegawai ke view index
-    // return view('index', ['pegawai' => $pegawai]);
-    return view('index_2', ['pegawai' => $pegawai]);
-    // return $pegawai;
+    return view('index', ['pegawai' => $pegawai]);
   }
 
   /* method untuk menampilkan view form tambah pegawai */
-  public function tambah(){
+  public function tambah_old(){
     return view('tambah');
   }
 
   /* model method - with query builder */
-  public function store(Request $request){
+  public function store_old(Request $request){
     // insert data to pegawai table
     DB::table('pegawai')->insert([
       'nama'    => $request->nama,
@@ -78,7 +122,7 @@ class PegawaiController extends Controller{
     return redirect('/pegawai');
   }
 
-  public function edit($id){
+  public function edit_old($id){
     $pegawai = DB::table('pegawai')->where('pegawai_id', $id)->first(); //retrieve one
     // $pegawai = DB::table('pegawai')->where('pegawai_id', $id)->get(); //can retrieve more than one
 
@@ -86,7 +130,7 @@ class PegawaiController extends Controller{
   }
 
   /* model method - update pegawai by id */
-  public function update(Request $request){
+  public function update_old(Request $request){
 
     //query to update data pegawai:
     DB::table('pegawai')->where('pegawai_id', $request->id)->update([
@@ -101,7 +145,7 @@ class PegawaiController extends Controller{
   }
 
   /* model method - delete pegawai by id */
-  public function delete($id){
+  public function delete_old($id){
     DB::table('pegawai')->where('pegawai_id', $id)->delete();
     return redirect('/pegawai');
   }
