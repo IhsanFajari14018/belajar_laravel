ee6d19ca06ffae83f2375aa6d97ff083ee2cc4e6
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-02-17 14:32:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -51,5 +51,14 @@ Route::post('/proses','MalasngodingController@proses');           // process
 //Route CRUD Eloquent
 Route::put('/pegawai/update/{id}', 'PegawaiController@update');
 
+//Route Guru
+Route::get('/guru', 'GuruController@index');
+Route::get('/guru/hapus/{id}', 'GuruController@hapus');
+Route::get('/guru/trash', 'GuruController@trash');
+Route::get('/guru/kembalikan/{id}', 'GuruController@kembalikan');
+Route::get('/guru/kembalikan_semua', 'GuruController@kembalikan_semua');
+Route::get('/guru/hapus_permanen/{id}', 'GuruController@hapus_permanen');
+Route::get('/guru/hapus_permanen_semua', 'GuruController@hapus_permanen_semua');
+
 //Route Pengguna
-Route::get('/pengguna','PenggunaController@index');             // Load data pengguna 
+Route::get('/pengguna','PenggunaController@index');             // Load data pengguna
