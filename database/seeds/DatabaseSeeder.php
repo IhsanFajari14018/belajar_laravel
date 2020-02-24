5a058edde374d257fe68c4c3c2d6734a76ad9a93
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-02-24 15:09:36 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+          ArticleSeeder::class,
+          GuruSeeder::class,
+          PegawaiSeeder::class,
+          PenggunaSeeder::class,
+          TagSeeder::class,
+          TeleponSeeder::class
+        ]);
     }
 }
