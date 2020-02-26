9ccff97c8ffa2d6f29adbe46f3ea3b4f8ab38210
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-02-26 14:37:11 +0700

    Revert "Update DatabaseSeeder.php"
    This reverts commit a5299f5a9477afb382455b6c7a2007f15bc295de.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,21 +4,20 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-  /**
-  * Seed the application's database.
-  *
-  * @return void
-  */
-  public function run()
-  {
-    $this->call([
-      ArticleSeeder::class,
-      GuruSeeder::class,
-      PegawaiSeeder::class,
-      PenggunaSeeder::class,
-      TagSeeder::class,
-      TeleponSeeder::class,
-      HadiahSeeder::class
-    ]);
-  }
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+          ArticleSeeder::class,
+          GuruSeeder::class,
+          PegawaiSeeder::class,
+          PenggunaSeeder::class,
+          TagSeeder::class,
+          TeleponSeeder::class
+        ]);
+    }
 }
