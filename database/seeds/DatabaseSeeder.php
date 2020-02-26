a5299f5a9477afb382455b6c7a2007f15bc295de
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-02-26 14:36:07 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,20 +4,21 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call([
-          ArticleSeeder::class,
-          GuruSeeder::class,
-          PegawaiSeeder::class,
-          PenggunaSeeder::class,
-          TagSeeder::class,
-          TeleponSeeder::class
-        ]);
-    }
+  /**
+  * Seed the application's database.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    $this->call([
+      ArticleSeeder::class,
+      GuruSeeder::class,
+      PegawaiSeeder::class,
+      PenggunaSeeder::class,
+      TagSeeder::class,
+      TeleponSeeder::class,
+      HadiahSeeder::class
+    ]);
+  }
 }
