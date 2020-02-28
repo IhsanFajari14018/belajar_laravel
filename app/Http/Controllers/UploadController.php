323294aaf44206d1b90c7abee279caa0c0ca26d3
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-02-28 16:46:35 +0700

    Create UploadController.php

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Gambar;
+
+class UploadController extends Controller{
+
+  /*
+  * Load upload page
+  */
+  public function upload(){
+    $gambar = Gambar::get();
+    return view('upload', ['gambar' => $gambar]);
+  }
+
+  /*
+  * Upload with storing file name to DB
+  */
+  public function proses_upload(Request $request){
+    $this->validate($request, [
+      'file'        => 'required|file|image|mimes:jpeg,jpg,png,PNG|max:2048',
+      'keterangan'  => 'required'
+    ]);
+
+    //menyimpan data file yang diupload ke variable $file
+    $file = $request->file('file');
+
+    $nama_file = time()."_".$file->getClientOriginalName();
+
+    //isi dengan nama folder tempat kemana file diupload
+    $tujuan_upload = 'file';
+    $file->move($tujuan_upload, $nama_file);
+
+    Gambar::create([
+      'file'        => $nama_file,
+      'keterangan'  => $request->keterangan
+    ]);
+
+    return redirect()->back();
+  }
+
+  /*
+  * Previous simple upload method
+  */
+  public function proses_upload_simple(Request $request){
+    $this->validate($request, [
+      'file'        =>  'required',
+      'keterangan'  => 'required'
+    ]);
+
+    //menyimpan data file yg diupload ke variable $file
+    $file = $request->file('file');
+
+    //nama file
+    echo 'File Name: '.$file->getClientOriginalName();
+    echo '<br>';
+
+    //ekstensi file
+    echo 'File Extension: '.$file->getClientOriginalExtension();
+    echo '<br>';
+
+    //real path
+    echo 'File Real Path'.$file->getRealPath();
+    echo '<br>';
+
+    //ukuran file
+    echo 'File Size: '.$file->getSize();
+    echo '<br>';
+
+    //tipe mime
+    echo 'File Mime Type: '.$file->getMimeType();
+
+    //isi dengan nama folder tempat kemana file diupload
+    $tujuan_upload = 'file/';
+
+    //upload file
+    //move( 'upload file path', 'file name')
+    $file->move($tujuan_upload, $file->getClientOriginalName());
+
+  }
+}
